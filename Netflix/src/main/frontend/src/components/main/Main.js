57dec31b61ac3a5a7f6750645994a828072a8906
miso062 <37644027+miso062@users.noreply.github.com>
2022-08-04 16:55:59 +0900

    0804_c1 #Main Page 버튼 수정!

--- Netflix/src/main/frontend/src/components/main/Main.js
+++ Netflix/src/main/frontend/src/components/main/Main.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Main = () => {
     return (
         <div>
-            gdgsdsd
+            <button className="border-[1px] p-5 m-4 bg-white text-[#141414]">
+                <Link to="/detail">Detail</Link>
+            </button>
+            <button className="border-[1px] p-5 m-4 bg-white text-[#141414]">
+                <Link to="/my">마이페이지</Link>
+            </button>
+            <button className="border-[1px] p-5 m-4 bg-white text-[#141414]">
+                <Link to="/service">go객센터</Link>
+            </button>
         </div>
     );
 };
