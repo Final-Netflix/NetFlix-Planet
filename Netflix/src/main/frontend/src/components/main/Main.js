31e4c0de44934531b60044a835f3306a460c6636
miso062 <37644027+miso062@users.noreply.github.com>
2022-08-04 17:04:46 +0900

    resource folder setting

--- Netflix/src/main/frontend/src/components/main/Main.js
+++ Netflix/src/main/frontend/src/components/main/Main.js
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 
 const Main = () => {
     return (
-        <div>
+        <>
+            <div id="header" className=''></div>
+            
             <button className="border-[1px] p-5 m-4 bg-white text-[#141414]">
                 <Link to="/detail">Detail</Link>
             </button>
@@ -13,7 +15,8 @@ const Main = () => {
             <button className="border-[1px] p-5 m-4 bg-white text-[#141414]">
                 <Link to="/service">go객센터</Link>
             </button>
-        </div>
+
+        </>
     );
 };
 
