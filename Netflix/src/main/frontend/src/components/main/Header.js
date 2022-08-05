211988802f7ec4061ca6b07422f9705927db22f9
miso062 <37644027+miso062@users.noreply.github.com>
2022-08-05 10:53:48 +0900

    0804_c1 #Header 작업중 거의 진행 사항 없음,,

--- Netflix/src/main/frontend/src/components/main/Header.js
+++ Netflix/src/main/frontend/src/components/main/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../image/logo.png';
 
 const Header = () => {
     return (
-        <div className='bg-gradient-to-b from-[#141414] to-transparent h-16'>
-            <img className='w-28' src={ logo }/>
+        <div className='c1-header bg-gradient-to-b from-[#141414] to-transparent h-16 text-[12px] text-white'>
+            <div className='c1-header-container flex pl-[37px] items-center z-[2]'>
+                <img className='w-28' src={ logo }/>
+                <ul className='m-0 p-0 flex items-center tracking-normal'>
+                    <li className='ml-[18px]'><Link to="">홈</Link></li>
+                    <li className='ml-[18px]'><Link to="">시리즈</Link></li>
+                    <li className='ml-[18px]'><Link to="">영화</Link></li>
+                    <li className='ml-[18px]'><Link to="">NEW! 요즘 대세 콘텐츠</Link></li>
+                    <li className='ml-[18px]'><Link to="">내가 찜한 콘텐츠</Link></li>
+                </ul>
+            </div>
         </div>
     );
 };
