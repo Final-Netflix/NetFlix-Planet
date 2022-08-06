0759afe878805d68146d762094a5e640c276736e
Steadily29 <wnscks19@naver.com>
2022-08-06 17:26:35 +0900

    0806_m1 #myPage view / 이메일주소변경(이메일인증,휴대폰인증) view

--- Netflix/src/main/frontend/tailwind.config.js
+++ Netflix/src/main/frontend/tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'small' : '500px',
+      'mid' : '740px',
+      'sm' : '640px',
+      'md' : '768px',
+      'lg' : '1024px',
+      'xl' : '1280px',
+      '2xl' : '1536px',
+    },
   },
   plugins: [],
 }
