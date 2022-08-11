685e6ed6c2973cdc68257c30fa9fbb6ae3c5f6e2
eunji <j-i@nate.com>
2022-08-11 20:19:09 +0900

    0811_k2#고객센터ui,스크립 수정

--- Netflix/src/main/frontend/src/components/service_center/ServiceCenterDetail.js
+++ Netflix/src/main/frontend/src/components/service_center/ServiceCenterDetail.js
@@ -48,10 +48,12 @@ const ServiceCenterDetail = () => {
                             <li>
                                 <a className="text-[#e50914] flex">
                                 <Link to='/service' >
+                                    
                                     <svg className='-mb-2' fill='#e50914' width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
                                         <polygon points="16.3515625 11.1015625 16.3515625 12.25 9.2421875 12.25 12.4960938 15.53125 11.6757812 16.3515625 7 11.6757812 11.6757812 7 12.4960938 7.8203125 9.2421875 11.1015625"></polygon>
                                     </svg>
-                                </Link>
+                                    
+                                </Link>          
                                     고객 센터 홈으로 돌아가기
                                 </a>
                             </li>
