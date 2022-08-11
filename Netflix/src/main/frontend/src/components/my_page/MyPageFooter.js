ea02508d764e48b8a1cde76a308ac902e067386e
suu1006 <1006pp@naver.com>
2022-08-11 16:02:54 +0900

    0811commit

---
+++ Netflix/src/main/frontend/src/components/my_page/MyPageFooter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../main/Footer.js';
+
+const MypageFooter = () => {
+    return (
+        <div>
+            <div className="border-solid pb-10 mt-10 border-t-[1px] h-2 border-zinc-700"></div> 
+                <div className = "pb-20">
+                    <Footer />
+                </div>
+        </div>
+    );
+};
+
+export default MypageFooter;
