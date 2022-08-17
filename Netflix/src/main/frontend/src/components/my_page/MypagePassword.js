adcd03cbd454311ed4dba2252bd648794b651f8c
Steadily29 <wnscks19@naver.com>
2022-08-17 11:48:50 +0900

    0817_m1 #Mypage 비밀번호 변경 기능구현 중(80%)완료

--- Netflix/src/main/frontend/src/components/my_page/MypagePassword.js
+++ Netflix/src/main/frontend/src/components/my_page/MypagePassword.js
@@ -1,6 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'css/myPage/MypageContainer.css'
 const MypagePassword = () => {
+    const [checked , setChecked] = useState(true)
+    
+    
+    //파란색 체크
+    const changeCheck = (e)=> {
+       const {checked} = e.target
+       setChecked(checked)
+       const box = document.createElement('style')
+       box.innerText=".ui-binary-input input[type=checkbox]:checked+label:after{ color: #0080ff; content: '\\e804'; font-family: nf-icon; font-size: 28px; left: -36px; top: -2px;}"
+        checked &&  document.body.appendChild(box) 
+    }
+    //저장 버튼(유효성 검사 및 비밀번호 변경)
+    const onSave = ()=> {
+        const id1 = document.getElementById('id_currentPassword')
+        const id2 = document.getElementById('id_newPassword')
+        const id3 = document.getElementById('id-confirmPassword')
+        //비밀번호 대조는 나중에 추가해야함
+        if(id1.value.length <=6) {
+            id1.focus()
+        }
+        if(id2.value.length <=6) {
+            document.getElementById('m1_inputError').innerText ='비밀번호는 6~60자 사이여야 합니다.'
+        }
+    
+    }
+   
+    //password input component
+    const PwdInput = ({id, content})=> {
+        const [pwd,setPwd] = useState('')
+        const changePwd = (e)=> {
+            setPwd(e.target.value)
+            document.getElementById(e.target.id).classList.add('hasText');
+            const classAll = document.getElementById(e.target.id)
+
+            if(classAll.value.length >=6) {
+                classAll.style.borderColor = 'green'
+                
+            }
+            if(classAll.value.length <6) {
+            classAll.style.borderColor = 'blue'
+            }
+
+            if(e.target.value.length ===0) {
+                document.getElementById(e.target.id).classList.remove('hasText');
+            }
+         
+        }
+        //focus out css
+        const onBlur = (e)=> {
+            const classAll = document.getElementById(e.target.id)
+            if(classAll.value.length <6) {
+               classAll.style.borderColor= '#8c8c8c'
+            }
+        }
+        return <li data-uia="field-currentPassword+wrapper" className="nfFormSpace">
+        <div data-uia="field-currentPassword+container" className="nfInput">
+            <div className="nfInputPlacement">
+                <label className="input_id" placeholder="currentPassword">
+                    <input type="password" onBlur={onBlur} onChange={changePwd} data-uia="field-currentPassword" value={pwd} name={id} className="nfTextField" id={`id_${id}`} tabIndex="0" autoComplete="off" maxLength="60" minLength="4" dir="" />
+                    <label htmlFor={`id_${id}`} className="placeLabel">{content}</label>
+                    <div className="checkedPwd"></div>
+                </label>
+            </div>
+            <div id="m1_inputError"></div>
+            {
+                id === 'currentPassword' &&  <div id="" className="inputCaption" data-uia=""><a data-ui="action_forgot_password" href="/loginhelp">비밀번호를 잊으셨나요? </a></div>
+            }
+        </div>
+    </li>
+    }
+
     return (
         <div>
             <div className="m1_bd">
@@ -8,59 +79,20 @@ const MypagePassword = () => {
                     <form className="change-password-form" data-uia="change-password-form" method="post" noValidate="">
                         <h1>비밀번호 변경</h1>
                         <ul className="simpleForm structural ui-grid">
-                            <li data-uia="field-currentPassword+wrapper" className="nfFormSpace">
-                                <div data-uia="field-currentPassword+container" className="nfInput">
-                                    <div className="nfInputPlacement">
-                                        <label className="input_id" placeholder="currentPassword">
-                                            <input type="password" data-uia="field-currentPassword" name="currentPassword" className="nfTextField" id="id_currentPassword" value="" tabIndex="0" autoComplete="off" maxLength="60" minLength="4" dir="" />
-                                            <label htmlFor="id_currentPassword" className="placeLabel">기존 비밀번호</label>
-                                        </label>
-                                    </div>
-                                    <div id="" className="inputCaption" data-uia=""><a data-ui="action_forgot_password" href="/loginhelp">비밀번호를 잊으셨나요? </a></div>
-                                </div>
-                            </li>
-                            <li data-uia="field-newPassword+wrapper" className="nfFormSpace">
-                                <div data-uia="field-newPassword+container" className="nfInput">
-                                    <div className="nfInputPlacement">
-                                        <label className="input_id" placeholder="newPassword">
-                                            <input type="password" data-uia="field-newPassword" name="newPassword" className="nfTextField" id="id_newPassword" value="" tabIndex="0" autoComplete="off" maxLength="60" minLength="6" dir="" />
-                                            <label htmlFor="id_newPassword" className="placeLabel">새 비밀번호(6~60자)</label>
-                                        </label>
-                                    </div>
-                                </div>
-                            </li>
-                            <li data-uia="field-confirmNewPassword+wrapper" className="nfFormSpace">
-                                <div data-uia="field-confirmNewPassword+container" className="nfInput">
-                                    <div className="nfInputPlacement">
-                                        <label className="input_id" placeholder="confirmNewPassword">
-                                            <input
-                                                type="password"
-                                                data-uia="field-confirmNewPassword"
-                                                name="confirmNewPassword"
-                                                className="nfTextField"
-                                                id="id_confirmNewPassword"
-                                                value=""
-                                                tabIndex="0"
-                                                autoComplete="off"
-                                                maxLength="60"
-                                                minLength="6"
-                                                dir=""
-                                            />
-                                            <label htmlFor="id_confirmNewPassword" className="placeLabel">새 비밀번호 재입력</label>
-                                        </label>
-                                    </div>
-                                </div>
-                            </li>
+                            <PwdInput id='currentPassword' content='기존 비밀번호' />
+                            <PwdInput id='newPassword' content='새 비밀번호(6~60자)' />
+                            <PwdInput id='confirmNewPassword' content='새 비밀번호 재입력' />
+                          
                             <li data-uia="field-requireAllDevicesSignIn+wrapper" className="nfFormSpace">
                                 <div className="ui-binary-input">
-                                    <input type="checkbox" className="" name="requireAllDevicesSignIn" id="cb_requireAllDevicesSignIn" value="true" tabIndex="0" checked="" data-uia="field-requireAllDevicesSignIn" />
-                                    <label htmlFor="cb_requireAllDevicesSignIn" data-uia="field-requireAllDevicesSignIn+label">모든 디바이스에서 새로운 비밀번호로 다시 로그인하셔야 합니다.</label>
-                                    <div className="helper"></div>
+                                    <input type="checkbox" name="requireAllDevicesSignIn" id="cb_requireAllDevicesSignIn" checked={checked} onChange={changeCheck} />
+                                    <label className="realCheck" htmlFor="cb_requireAllDevicesSignIn" data-uia="field-requireAllDevicesSignIn+label">모든 디바이스에서 새로운 비밀번호로 다시 로그인하셔야 합니다.</label>
+                                    
                                 </div>
                             </li>
                         </ul>
                         <div className="nf-btn-bar change-password-buttons">
-                            <button id="btn-save" type="submit" autoComplete="off" tabIndex="0" className="nf-btn nf-btn-primary nf-btn-retro nf-btn-small" data-uia="action_save-password" placeholder="">저장</button>
+                            <button id="btn-save" type="button" onClick={onSave}  autoComplete="off" tabIndex="0" className="nf-btn nf-btn-primary nf-btn-retro nf-btn-small" data-uia="action_save-password" placeholder="">저장</button>
                             <button id="btn-cancel" type="button" autoComplete="off" tabIndex="0" className="nf-btn nf-btn-secondary nf-btn-retro nf-btn-small" data-uia="action_cancel-change-password" placeholder="">취소</button>
                         </div>
                     </form>
