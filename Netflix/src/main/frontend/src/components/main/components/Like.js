d883e10d36bea0aee08be3e65bfe5a52bbd605e0
suu1006 <1006pp@naver.com>
2022-08-19 16:49:52 +0900

    0819_c1 #search 검색 페이지 / 검색 페이지 진행중

---
+++ Netflix/src/main/frontend/src/components/main/components/Like.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from './Footer';
+import Header from './Header';
+import View from './View';
+
+const Like = () => {
+    return (
+        <div>
+            <View/>
+        </div>
+    );
+};
+
+export default Like;
