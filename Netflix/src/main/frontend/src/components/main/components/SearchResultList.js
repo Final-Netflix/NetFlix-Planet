f317b31cedfb90aef0a288d4dc56fd7d9a4bdf0c
suu1006 <1006pp@naver.com>
2022-08-26 10:45:53 +0900

    0826_c1 #search 검색 페이지 진행중 / 검색페이지 로고 수정중

--- Netflix/src/main/frontend/src/components/main/components/SearchResultList.js
+++ Netflix/src/main/frontend/src/components/main/components/SearchResultList.js
@@ -7,7 +7,7 @@ const SearchResultList = ({ searchList }) => {
     //[{"adult":false,"backdrop_path":"/7lmBufEG7P7Y1HClYK3gCxYrkgS.jpg","genre_ids":[12,28,878],"id":10138,"original_language":"en","original_title":"Iron Man 2",
     
     return (
-        <div className="c1-galleryContent">
+        <div className="c1-galleryContent mt-3">
             <div className="c1-galleryLockups">
                 <div className="c1-rowContainer c1-rowContainer_title_card" id="row-0">
                     <div className="c1-ptrack-container">
