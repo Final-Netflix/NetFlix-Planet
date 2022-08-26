7d1be1f5d37a6e33f18f120a7a71dffafadc47f3
miso062 <37644027+miso062@users.noreply.github.com>
2022-08-26 09:19:12 +0900

    0826_c1 #classification for recommend 작업중!

---
+++ Netflix/src/main/java/classification/dao/ClassificationDAO.java
@@ -0,0 +1,11 @@
+package classification.dao;
+
+import java.util.List;
+
+import classification.bean.ClassificationDTO;
+
+public interface ClassificationDAO {
+
+	public List<ClassificationDTO> getClassifications();
+
+}
