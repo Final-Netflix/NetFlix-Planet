0c3610b57f12e46aa7ec551df726eef7ce2539dd
eunji <j-i@nate.com>
2022-08-29 11:19:34 +0900

    0829_#마이페이휴대폰코드받기

--- Netflix/src/main/frontend/src/components/user/SignUp.js
+++ Netflix/src/main/frontend/src/components/user/SignUp.js
@@ -28,7 +28,7 @@ const SignUp = () => {
                                     <a className="c1-social-link" href="https://www.facebook.com/NetflixKR" target="_blank" aria-label="facebook">
                                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="c1-svg-icon c1-svg-icon-facebook-logo">
                                             <path fillRule="evenodd" clipRule="evenodd" d="M13.9868 13.1621V21.9841H10.0418V13.1621H6.8418V9.51207H10.0468V6.73207C10.0468 3.56707 11.9318 1.82007 14.8148 1.82007C15.7616 1.83321 16.7061 1.91577 17.6408 2.06707V5.17307H16.0448C15.4952 5.10007 14.9422 5.28088 14.5419 5.66447C14.1415 6.04807 13.9373 6.59284 13.9868 7.14507V9.51207H17.4868L16.9278 13.1621H13.9868Z" fill="currentColor"></path>
-                                        </svg>
+                                        </svg> 
                                     </a>
                                     <a className="c1-social-link" href="https://www.instagram.com/netflixkr/" target="_blank" aria-label="instagram">
                                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="c1-svg-icon c1-svg-icon-instagram-logo">
--- Netflix/src/main/java/user/controller/UserController.java
+++ Netflix/src/main/java/user/controller/UserController.java
@@ -88,8 +88,4 @@ public class UserController {
 	}
 	
 	
-	
 }
-
-
-
